116ef354af3596b07bfcbced7cc7f5106a4d3809
TandlZsanett <tandlzsanett2005@gmail.com>
2022-04-23 09:55:44 +0200

    9:54
    Megcsináltam a mondatösszerakősat, egyenlőre külön fájlba van és megakarom mutatni hogy mivel szenvedtem és mi nem működött tegnap ezért ki comment-eltem a rosszat.

--- probaaszoosszerakoshoz.py
+++ probaaszoosszerakoshoz.py
@@ -1,7 +1,6 @@
 import random
 
-mondathoz=open('osszerak/mondatok.txt','r',encoding='utf-8')
-
+'''
 osszerakpontok=0
 
 ez=[]
@@ -44,6 +43,55 @@ for x in range(len(mon)-1):
         osszerakpontok+=1
     else: print('Sajnos nem ez a megoldás :(')
 
+
+
+'''
+
+
+
+class mondat():
+    def __init__(self,elso,masodik,harmadik,negyedik):
+        self.elso=elso
+        self.masodik=masodik
+        self.harmadik=harmadik
+        self.negyedik=negyedik
+
+egesz=[]
+mon=[]
+
+mondathoz=open('osszerak/mondatok.txt','r',encoding='utf-8')
+for sor in mondathoz:
+    sor=sor.replace('\n','')
+    d=sor.split(';')
+    obj=mondat(d[0],d[1],d[2],d[3])
+    mon.append(obj)
+    sor=sor.replace(';','')
+    egesz.append(sor)
+
+
+print('Ebben a játékban szétszedtunk 4db-ra egy mondatod és megcsréltük a szavait. A szavakat fogod látni a képernyőn. írd be a helyes mondatot')
+osszerakpontok=0
+
+fel=1
+
+for x in range(len(mon)):
+    ossze=[]
+    kulon=[]
+    while len(ossze)!=4:
+        kulon.append(mon[x].elso)
+        kulon.append(mon[x].masodik)
+        kulon.append(mon[x].harmadik)
+        kulon.append(mon[x].negyedik)
+        randba=random.choice(kulon)
+        if randba not in ossze: ossze.append(randba)
+    print('{}. feladat: {} {} {} {}'.format(fel,ossze[0],ossze[1],ossze[2],ossze[3]))
+    be=input('\n\tMegfejtés: ')
+    print()
+    if be==egesz[x]:
+        print('A megoldás helyes')
+        osszerakpontok+=1
+    else: print('A megoldás nem helyes :(\n A helyes megfejtés: {}\n\n'.format(egesz[x])) 
+    fel+=1
 
 def mennyirejo(a):
     if a>20: return 'Nagyon ügyes voltál'
