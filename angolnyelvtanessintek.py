704c4d67b29f86116083e52fbeed69c159713828
Janka172 <jankahegedus872@gmail.com>
2022-05-02 10:22:50 +0200

    Update angolnyelvtanessintek.py

--- angolnyelvtanessintek.py
+++ angolnyelvtanessintek.py
@@ -1,26 +1,30 @@
 class angszint:
     def __init__(self,ker,val1,val2,val3,val4,mego):
-        self.ker==ker
-        self.val1==val1
-        self.val2==val2
-        self.val3==val3
-        self.val4==val4
-        self.mego==mego
+        self.ker=ker
+        self.val1=val1
+        self.val2=val2
+        self.val3=val3
+        self.val4=val4
+        self.mego=mego
 
-ang=open('osszerak/angolszint.txt','r',encoding='utf-8')
-angL=[]
+f=open('osszerak/angolszint.txt','r',encoding='utf-8')
+L=[]
 
-
-for sor in ang:
-    sor=sor.replace('\n','')
-    d=sor.split(';')
+L=[]
+f=open("osszerak/angolszint.txt","r",encoding="utf-8")
+for sor in f:
+    sor=sor.replace("\n","")
+    d=sor.split(";")
     obj=angszint(d[0],d[1],d[2],d[3],d[4],int(d[5]))
-    angL.append(obj)
+    L.append(obj)
+f.close
+
 
 me=0
-szint=input('Szeretnéd felmérni a nyelvi színtedet angolból? [i/n]')
+szint=input('Szeretnéd felmérni a nyelvi színtedet angolból? [i/n] ')
 if szint=='i':
     while me>=51 or me<=16:
-        me=int(input('Hány kérdéses tesztet szeretnél? [legkisebb 15, legnagyobb 50]'))
+        me=int(input('Hány kérdéses tesztet szeretnél? [legkisebb 15, legnagyobb 50] '))
         
     
+
