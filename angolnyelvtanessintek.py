5e892f7d8ce02800726c14c1b92b5725ba5b23fc
Janka172 <jankahegedus872@gmail.com>
2022-05-02 10:56:53 +0200

    MIÉRT NEM JÓÓÓÓ?!?!?!?!??!?!?

--- angolnyelvtanessintek.py
+++ angolnyelvtanessintek.py
@@ -1,4 +1,4 @@
-class angszint:
+class angszint():
     def __init__(self,ker,val1,val2,val3,val4,mego):
         self.ker==ker
         self.val1==val1
@@ -7,6 +7,15 @@ class angszint:
         self.val4==val4
         self.mego==mego
 
+
+
+def szintek(a,b):
+    szaz=a/b
+    if szaz<=100 and szaz>=85:
+        return 'Felső fokura készülő szint'
+    elif szaz>75: return 'Erős középhaladó szint'
+    elif szaz>65: return 'Középhaladó szint'
+    elif szaz>45: return 'Kezdő szint' 
 ang=open('osszerak/angolszint.txt','r',encoding='utf-8')
 angL=[]
 
@@ -16,11 +25,16 @@ for sor in ang:
     d=sor.split(';')
     obj=angszint(d[0],d[1],d[2],d[3],d[4],int(d[5]))
     angL.append(obj)
-
+ang.close()
 me=0
+szintpont=0
 szint=input('Szeretnéd felmérni a nyelvi színtedet angolból? [i/n]')
 if szint=='i':
     while me>=51 or me<=16:
         me=int(input('Hány kérdéses tesztet szeretnél? [legkisebb 15, legnagyobb 50]'))
     for x in range(me):
-        
+        print('{}'.format(angL[x].ker))
+        be=int(input((' \n\t1.{}  \n\t2. {} \n\t3. {} \n\t4. {} \n\t Megoldás: '.format(angL[x].val1,angL[x].val2,angL[x].val3,angL[x].val4))))
+        if be==angL[x].mego:
+            szintpont+=1
+    print('{}/{} helyes válaszod volt. Az angol szinted: {}. Ez az eredmény nem biztosan a szintedet prezentálja, ez csak egy általános felmérés.')
